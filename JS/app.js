68623ddaf3768127d17181a86f6959f75a1b3de8
Orozimbo1 <matheusorozimbo5@gmail.com>
2022-06-17 23:04:41 -0300

    Esboço do projeto

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,28 @@
+let ofert_email = document.getElementById("ofert_email")
+let register = document.getElementById("register")
+
 console.log("funcionando");
+
+//zerando valor no campo de email
+
+ofert_email.addEventListener("click", () => {
+    ofert_email.value = ""
+})
+
+// efeito temporario
+
+register.addEventListener("mousemove", () => {
+    register.style.backgroundColor = "#f11319fe"
+})
+
+register.addEventListener("mouseleave", () => {
+    register.style.backgroundColor = "#ff002bfd"
+})
+
+register.addEventListener("mousedown", () => {
+    register.style.backgroundColor = "#ac132cfd"
+})
+
+register.addEventListener("mouseup", () => {
+    register.style.backgroundColor = "#ff002bfd"
+})
