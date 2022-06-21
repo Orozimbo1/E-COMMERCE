704f336b84280cd68352c3b2722a5c51ecbba52f
Orozimbo1 <matheusorozimbo5@gmail.com>
2022-06-21 17:49:41 -0300

    esboço pagina de perfil criado

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,15 @@ let ofert_email = document.getElementById("ofert_email")
 let principalPage = document.getElementById("principalPage")
 let pageLogin = document.getElementById("pageLogin")
 let pageRegister = document.getElementById("pageRegister")
+let pagePrincipalPerfil = document.getElementById("pagePrincipalPerfil")
+let pageCompras = document.getElementById("pageCompras")
+let pageProdutos = document.getElementById("pageProdutos")
+let pageFavoritos = document.getElementById("pageFavoritos")
+let pagePagamento = document.getElementById("pagePagamento")
+let pageConfiguracoes = document.getElementById("pageConfiguracoes")
+let pageAjuda = document.getElementById("pageAjuda")
+
+let titlePage = document.querySelector("title")
 
 
 console.log("funcionando");
@@ -18,6 +27,7 @@ function paginaLogin() {
     principalPage.classList.add("ocult")
     pageRegister.classList.add("ocult")
     pageLogin.classList.remove("ocult")
+    titlePage.innerHTML = "Login"
 }
 
 //Redirecionando para a pagina de Cadastro
@@ -26,6 +36,7 @@ function paginaCadastro(){
     principalPage.classList.add("ocult")
     pageLogin.classList.add("ocult")
     pageRegister.classList.remove("ocult")
+    titlePage.innerHTML = "Cadastre-se"
 }
 
 // redirecionando para a pagina Principal
@@ -34,4 +45,84 @@ function paginaHome() {
     principalPage.classList.remove("ocult")
     pageLogin.classList.add("ocult")
     pageRegister.classList.add("ocult")
+    titlePage.innerHTML = "OnlineShop"
+}
+
+//TOPICOS PAGINA DE PERFIL//
+
+function paginaPerfilPrincipal() {
+    pagePrincipalPerfil.classList.remove("ocult")
+    pageCompras.classList.add("ocult")
+    pageProdutos.classList.add("ocult") 
+    pageFavoritos.classList.add("ocult")
+    pagePagamento .classList.add("ocult")
+    pageConfiguracoes.classList.add("ocult") 
+    pageAjuda.classList.add("ocult")
+     
+}
+
+function paginaCompras() {
+    pagePrincipalPerfil.classList.add("ocult")
+    pageCompras.classList.remove("ocult")
+    pageProdutos.classList.add("ocult") 
+    pageFavoritos.classList.add("ocult")
+    pagePagamento .classList.add("ocult")
+    pageConfiguracoes.classList.add("ocult") 
+    pageAjuda.classList.add("ocult")
+     
+}
+
+function paginaProdutos() {
+    pagePrincipalPerfil.classList.add("ocult")
+    pageCompras.classList.add("ocult")
+    pageProdutos.classList.remove("ocult") 
+    pageFavoritos.classList.add("ocult")
+    pagePagamento .classList.add("ocult")
+    pageConfiguracoes.classList.add("ocult") 
+    pageAjuda.classList.add("ocult")
+     
+}
+
+function paginaFavoritos() {
+    pagePrincipalPerfil.classList.add("ocult")
+    pageCompras.classList.add("ocult")
+    pageProdutos.classList.add("ocult") 
+    pageFavoritos.classList.remove("ocult")
+    pagePagamento .classList.add("ocult")
+    pageConfiguracoes.classList.add("ocult") 
+    pageAjuda.classList.add("ocult")
+     
+}
+
+function paginaPagamento() {
+    pagePrincipalPerfil.classList.add("ocult")
+    pageCompras.classList.add("ocult")
+    pageProdutos.classList.add("ocult") 
+    pageFavoritos.classList.add("ocult")
+    pagePagamento .classList.remove("ocult")
+    pageConfiguracoes.classList.add("ocult") 
+    pageAjuda.classList.add("ocult")
+     
+}
+
+function paginaConfiguracoes() {
+    pagePrincipalPerfil.classList.add("ocult")
+    pageCompras.classList.add("ocult")
+    pageProdutos.classList.add("ocult") 
+    pageFavoritos.classList.add("ocult")
+    pagePagamento .classList.add("ocult")
+    pageConfiguracoes.classList.remove("ocult") 
+    pageAjuda.classList.add("ocult")
+     
+}
+
+function paginaAjuda() {
+    pagePrincipalPerfil.classList.add("ocult")
+    pageCompras.classList.add("ocult")
+    pageProdutos.classList.add("ocult") 
+    pageFavoritos.classList.add("ocult")
+    pagePagamento .classList.add("ocult")
+    pageConfiguracoes.classList.add("ocult") 
+    pageAjuda.classList.remove("ocult")
+     
 }
